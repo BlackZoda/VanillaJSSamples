903200d94dff86b728b84a8aae68f27fe5af9b54
Ivar Abusdal <ivar.abusdal@designerkontoret.no>
2024-01-15 00:51:47 +0100

    file upload to server working

--- upload-service/public/script.js
+++ upload-service/public/script.js
@@ -1,9 +1,54 @@
 const btnUpload = document.getElementById("file-button");
 const divOutput = document.getElementById("div-output");
-const file = document.getElementById("file");
+const fileUploads = document.getElementById("file");
 
 btnUpload.addEventListener("click", (e) => {
   e.preventDefault();
 
-  divOutput.innerText = "Hi";
+  const endpoint = "http://localhost:8080/upload";
+  const file = fileUploads.files[0];
+  const fileName = file.name;
+  const fileId = uuidv4();
+  const fileReader = new FileReader();
+
+  fileReader.onload = async (loadEvent) => {
+    const byteLength = loadEvent.target.result.byteLength;
+    const chunkSize = 5000;
+    const chunkCount = Math.ceil(byteLength / chunkSize);
+
+    console.log(`Byte Length: ${byteLength}`);
+    console.log(`Chunk Size: ${chunkSize}`);
+    console.log(`Chunk Count: ${chunkCount}`);
+    console.log(`File name: ${fileName}`);
+    console.log(`File ID: ${fileId}`);
+
+    for (let chunkId = 0; chunkId < chunkCount; chunkId++) {
+      const low = chunkId * chunkSize;
+      const high = low + chunkSize;
+      const chunk = loadEvent.target.result.slice(low, high);
+
+      await fetch(endpoint, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/octet-stream",
+          "Content-Length": chunkSize,
+          // Custom headers
+          "File-Name": fileName,
+          "File-Id": fileId,
+        },
+        body: chunk,
+      });
+    }
+  };
+
+  fileReader.readAsArrayBuffer(file);
 });
+
+function uuidv4() {
+  return "10000000-1000-4000-8000-100000000000".replace(/[018]/g, (c) =>
+    (
+      c ^
+      (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))
+    ).toString(16),
+  );
+}
--- upload-service/server.js
+++ upload-service/server.js
@@ -3,30 +3,50 @@ const fs = require("node:fs");
 const path = require("node:path");
 
 const server = http.createServer((req, res) => {
-  const filePath = path.join(
-    __dirname,
-    "public",
-    req.url === "/" ? "index.html" : req.url,
-  );
+  console.log(req.url);
+  if (req.url === "/upload") {
+    res.writeHead(200, {
+      "Content-Type": "text/plain",
+    });
 
-  fs.readFile(filePath, (err, data) => {
-    if (err) {
-      res.writeHead(404, { "Content-Type": "text/plain" });
-      return res.end("404 Not Found");
-    }
+    // Custom headers
+    const fileName = req.headers["file-name"];
+    const fileId = req.headers["file-id"];
 
-    const ext = path.extname(filePath);
-    const contentType =
-      {
-        ".html": "text/html",
-        ".js": "text/javascript",
-        ".css": "text/css",
-      }[ext] || "text/plain";
+    req.on("data", (chunk) => {
+      fs.appendFileSync(`${fileId}_${fileName}`, chunk);
+      console.log(`Chunk length received: ${chunk.length}`);
+    });
 
-    res.writeHead(200, { "Content-Type": contentType });
+    res.end("Working!");
+  }
 
-    res.end(data);
-  });
+  if (req.url !== "/upload") {
+    const filePath = path.join(
+      __dirname,
+      "public",
+      req.url === "/" ? "index.html" : req.url,
+    );
+
+    fs.readFile(filePath, (err, data) => {
+      if (err) {
+        res.writeHead(404, { "Content-Type": "text/plain" });
+        return res.end("404 Not Found");
+      }
+
+      const ext = path.extname(filePath);
+      const contentType =
+        {
+          ".html": "text/html",
+          ".js": "text/javascript",
+          ".css": "text/css",
+        }[ext] || "text/plain";
+
+      res.writeHead(200, { "Content-Type": contentType });
+
+      res.end(data);
+    });
+  }
 });
 
 const PORT = 8080;
