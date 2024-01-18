5a7f4e3691428d367891cbb0295fcce8619593ff
Ivar Abusdal <ivar.abusdal@designerkontoret.no>
2024-01-18 13:37:01 +0100

    file change event and refactoring

--- node/file-system/main.js
+++ node/file-system/main.js
@@ -1,33 +1,44 @@
-// const fsPromises = require("fs/promises");
+// alternative to fs.watch
 const chokidar = require("chokidar");
 const fsPromises = require("node:fs/promises");
 
-(async () => {
-  chokidar.watch("./commands.txt").on("change", async (path) => {
-    console.log("The file was changed.");
+async function main() {
+  commandFile = await fsPromises.open("./commands.txt", "r");
 
-    const commandFile = await fsPromises.open("./commands.txt", "r");
-
-    const buffer = Buffer.alloc((await commandFile.stat()).size);
-
-    const fileProps = {
-      buffer: buffer,
-      offset: 0,
-      length: buffer.byteLength,
-      position: 0,
-    };
-
-    const content = await commandFile.read(
-      fileProps.buffer,
-      fileProps.offset,
-      fileProps.length,
-      fileProps.position,
-    );
-
-    console.log(content);
-
-    await commandFile.close();
+  commandFile.on("change", (path) => {
+    console.log(`${path} changed!`);
   });
-})();
 
-// open => file descriptor to memory => a unique number
+  try {
+    chokidar.watch("./commands.txt").on("change", async (path) => {
+      commandFile.emit("change", path);
+
+      const buffer = Buffer.alloc((await commandFile.stat()).size);
+
+      const fileProps = {
+        buffer: buffer,
+        offset: 0, // location at which we want to start filling our buffer
+        length: buffer.byteLength, // bytes we want to read
+        position: 0, // positon that we start reading the file from
+      };
+
+      // reading the whole content
+      const content = await commandFile.read(
+        fileProps.buffer,
+        fileProps.offset,
+        fileProps.length,
+        fileProps.position,
+      );
+
+      console.log(content);
+    });
+
+    await new Promise(() => {});
+  } catch (e) {
+    console.error(e);
+  } finally {
+    await commandFile.close();
+  }
+}
+
+main();
