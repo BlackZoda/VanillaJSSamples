34c69bb58149cb540fa06907e6439b4a2dbcceab
Ivar Abusdal <ivar.abusdal@designerkontoret.no>
2024-02-09 11:38:13 +0100

    custom node write stream written

--- node/streams/examples/custom-writable/customWritable.js
+++ node/streams/examples/custom-writable/customWritable.js
@@ -7,6 +7,9 @@ class FileWriteStream extends Writable {
 
     this.fileName = fileName;
     this.fd = null;
+    this.chunks = [];
+    this.chunkSize = 0;
+    this.writesCount = 0;
   }
 
   // This will run after the constructor,
@@ -24,13 +27,43 @@ class FileWriteStream extends Writable {
   }
 
   _write(chunk, encoding, callback) {
-    console.log(this.fd);
-    callback(); // drain event - don't emit events from child classes
+    this.chunks.push(chunk);
+    this.chunkSize += chunk.length;
+
+    if (this.chunkSize > this.writableHighWaterMark) {
+      fs.write(this.fd, Buffer.concat(this.chunks), (err) => {
+        if (err) {
+          return callback(err);
+        }
+        this.chunks = [];
+        this.chunkSize = 0;
+        ++this.writesCount;
+        callback();
+      });
+    } else {
+      callback();
+    }
   }
 
-  // _final() {}
+  _final(callback) {
+    fs.write(this.fd, Buffer.concat(this.chunks), (err) => {
+      if (err) return callback(err);
 
-  // _destroy() {}
+      this.chunks = [];
+      callback();
+    });
+  }
+
+  _destroy(error, callback) {
+    console.log("Number of writes:", this.writesCount);
+    if (this.fd) {
+      fs.close(this.fd, (err) => {
+        callback(err || error);
+      });
+    } else {
+      callback(error);
+    }
+  }
 }
 
 const stream = new FileWriteStream({
@@ -38,5 +71,9 @@ const stream = new FileWriteStream({
   fileName: "test.txt",
 });
 
-stream.write(Buffer.from("example string"));
-// stream.end(Buffer.from("last write"));
+stream.write(Buffer.from("Example string.\n"));
+stream.end(Buffer.from("Last write.\n"));
+
+stream.on("finish", () => {
+  console.log("Stream finished!");
+});
